8fccc2cbca8208a0ba0992f165d97146ee2e29ac
Jeremy M. Reed <reeje76@gmail.com>
2016-06-22 10:33:33 -0600

    Implement methods.

--- src/main/java/wordcount/WordCount.java
+++ src/main/java/wordcount/WordCount.java
@@ -1,6 +1,7 @@
 package wordcount;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
@@ -9,11 +10,31 @@ import exceptions.NotImplementedException;
 public class WordCount {
 	
 	public List<String> parse(String input) {
-		throw new NotImplementedException();
+		String[] tokens = input.split( "\\s+" );
+		List<String> sanitizedStrings = new ArrayList<String>();
+		for ( String token : tokens ) {
+			String sanitizedString = this.sanitize( token );
+			if ( !sanitizedString.isEmpty( ) ) {
+				sanitizedStrings.add( sanitizedString );
+			}
+		}
+		return sanitizedStrings;
 	}
 	
 	public Map<String, Integer> phrase(String input) {
-		throw new NotImplementedException();
+		Map<String, Integer> results = new HashMap<String, Integer>();
+		List<String> sanitizedStrings = this.parse( input );
+		
+		for ( String sanitizedString : sanitizedStrings ) {
+			Integer value = results.get( sanitizedString );
+			if ( value == null ) {
+				results.put( sanitizedString, 1 );
+			} else {
+				value +=1;
+				results.replace( sanitizedString, value );
+			}
+		}
+		return results;
 	}
 	
 	private String sanitize(String input) {
@@ -23,6 +44,6 @@ public class WordCount {
 				sb.append( input.charAt( i ) );
 			}
 		}
-		return sb.toString( );
+		return sb.toString( ).toLowerCase( );
 	}
 }
