13017ba487f51650696df872fc29bb31fbc0fbc5
Jeremy M. Reed <reeje76@gmail.com>
2016-06-22 10:21:32 -0600

    Stub methods.

--- src/main/java/wordcount/WordCount.java
+++ src/main/java/wordcount/WordCount.java
@@ -1,11 +1,28 @@
 package wordcount;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Map;
 
 import exceptions.NotImplementedException;
 
 public class WordCount {
+	
+	public List<String> parse(String input) {
+		throw new NotImplementedException();
+	}
+	
 	public Map<String, Integer> phrase(String input) {
 		throw new NotImplementedException();
+	}
+	
+	private String sanitize(String input) {
+		StringBuilder sb = new StringBuilder();
+		for ( int i = 0 ; i < input.length( ) ; i++ ) {
+			if (Character.isLetterOrDigit( input.charAt( i ) ) ) {
+				sb.append( input.charAt( i ) );
+			}
+		}
+		return sb.toString( );
 	}
 }
