71e0ddb51aadc800efcee093889a2b0db3e659d5
Jeremy M. Reed <reeje76@gmail.com>
2016-06-22 09:39:26 -0600

    Stub out phrase(...) method.

--- src/main/java/wordcount/WordCount.java
+++ src/main/java/wordcount/WordCount.java
@@ -1,3 +1,11 @@
 package wordcount;
+
+import java.util.Map;
+
+import exceptions.NotImplementedException;
+
 public class WordCount {
+	public Map<String, Integer> phrase(String input) {
+		throw new NotImplementedException();
+	}
 }
